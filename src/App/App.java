08f012915be90ba4b0f0c751b1ea8bf61c99bee5
Kevin Marquez <yean.marquez@correo.tdea.edu.co>
2024-08-19 18:44:08 -0500

    version 1

--- src/App/App.java
+++ src/App/App.java
@@ -11,7 +11,7 @@ public class App {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("pene");
+        System.out.println("pen");
     }
     
 }
