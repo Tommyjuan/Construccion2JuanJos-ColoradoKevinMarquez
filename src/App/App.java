9aee8bd9b74fdc11c56be073fde32d5b4dde1d7e
Kevin Marquez <yean.marquez@correo.tdea.edu.co>
2024-08-19 22:05:47 -0500

    conectado a base de datos

--- src/App/App.java
+++ src/App/App.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package App;
-
+import App.config.MYSQLConnection;
 
 public class App {
 
@@ -11,7 +11,13 @@ public class App {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("pen");
+        MYSQLConnection controller = new MYSQLConnection();
+		try {
+			controller.getConnection();
+			//MYSQLConnection.getConnection();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
     }
     
 }
